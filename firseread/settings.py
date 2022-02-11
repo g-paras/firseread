838a64714b964699b82bf773b8e33482a68e2a35
Paras Gupta <guptaparas039@gmail.com>
2022-02-11 15:36:51 +0530

    add whitenoise middleware

--- firseread/settings.py
+++ firseread/settings.py
@@ -49,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',  # added manually
 ]
 
 ROOT_URLCONF = 'firseread.urls'
