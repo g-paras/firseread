6db6f4726c83eccb2785458149289afd16279c9d
Paras Gupta <guptaparas039@gmail.com>
2022-02-11 14:16:13 +0530

    update project name

--- firseread/asgi.py
+++ firseread/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for firsebooking project.
+ASGI config for firseread project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'firsebooking.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'firseread.settings')
 
 application = get_asgi_application()
--- firseread/wsgi.py
+++ firseread/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for firsebooking project.
+WSGI config for firseread project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'firsebooking.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'firseread.settings')
 
 application = get_wsgi_application()
